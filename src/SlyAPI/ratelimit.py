ed078544bfe268f19dd3926ed097d206df41b6bd
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2021-11-26 02:51:57 -0800

    start on ratelimits

---
+++ src/SlyAPI/ratelimit.py
@@ -0,0 +1,13 @@
+from .webapi import *
+import functools
+
+Endpoint = Callable[[WebAPI, T], Coro[U]]
+
+def ratelimit(bucket: str, bucket_params: list[str]) -> Callable[[Endpoint[Any, T]], Endpoint[Any, T]]:
+    def decorator(func: Endpoint[Any, T]) -> Endpoint[Any, T]:
+        @functools.wraps(func)
+        async def wrapper(self: WebAPI, params: dict[str, Any]) -> T:
+            # TODO
+            return await func(self, params)
+        return wrapper
+    return decorator
